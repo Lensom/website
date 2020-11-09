16d0504c900ecc731f6cdd6c2f7be8dcd0dddccc
Lensom <magurawork@gmail.com>
2020-11-09 22:01:35 +0300

    Add page projects

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from 'components/App';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Projects from 'pages/projects';
-import Contact from 'pages/contact';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <BrowserRouter>
