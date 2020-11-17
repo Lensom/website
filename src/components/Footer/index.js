fabd599eaefa47615865b1a36630b694bfb99ec1
Lensom <magurawork@gmail.com>
2020-11-17 22:18:23 +0300

    Add ring for main page, add theme icon

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import SocialNetworks from 'components/SocialNetworks';
 import classes from './Styles.module.scss';
 
+import themeImg from './images/theme.svg';
+
 const Footer = () => (
   <div className={classes.footer}>
     <div className={classes.footer__wrapper}>
       <SocialNetworks />
-      <span className={classes.game}>Mini game</span>
+      <span className={classes.theme}>
+        <img src={themeImg} alt="Change theme color"/>
+      </span>
     </div>
   </div>
 )
--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import classes from './Styles.module.scss';
+import classNames from 'classnames';
 
 const Main = () => {
   return (
@@ -10,6 +11,14 @@ const Main = () => {
         </div>
         <div className="subtitle__wrapper">
           <h2 className={classes.subtitle}>Hi, I'm Alexey Rybalko · Web Developer based in Ukraine</h2>
+        </div>
+      </div>
+      <div className="photo">
+        <div className={classNames([classes.ring, classes.ringOne])}>
+          <img src="ring.png" alt="" />
+        </div>
+        <div className={classNames([classes.ring, classes.ringTwo])}>
+          <img src="ring.png" alt="" />
         </div>
       </div>
     </div>
