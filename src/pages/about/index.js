045eb4ce28956b94b0aed672ed802080abee29a0
Lensom <magurawork@gmail.com>
2020-11-18 22:12:29 +0300

    Add Gsap for about page

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -8,44 +8,41 @@ import gsap from "gsap";
 
 const About = () => {
 
-  // const body = document.querySelector('body')
+  const body = document.querySelector('body')
+	let cx = window.innerWidth / 2
+	let cy = window.innerHeight / 2
+	let clientX = null;
+	let clientY = null;
 
-	// let cx = window.innerWidth / 2
-	// let cy = window.innerHeight / 2
+	body.addEventListener('mousemove', e => {
 
-	// body.addEventListener('mousemove', e => {
+		clientX = e.pageX
+		clientY = e.pageY
 
-	// 	clientX = e.pageX
-	// 	clientY = e.pageY
+		console.log(clientX, clientY)
 
-	// 	request = requestAnimationFrame(updateMe)
+		let request = requestAnimationFrame(updateMe)
 
-	// })
+	})
 
-	// function updateMe() {
+	function updateMe() {
 
-	// 	dx     = clientX - cx
-	// 	dy     = clientY - cy
-	// 	tiltx  = dy / cy
-	// 	tilty  = dx / cx
-	// 	radius = Math.sqrt(Math.pow(tiltx, 2) + Math.pow(tilty, 2))
-	// 	degree = radius * 12
-	// 	gsap.to('.content', 1, { transform: `rotate3d( ${tiltx}, ${tilty}, 0, ${degree}deg )` })
+		let dx     = clientX - cx
+		let dy     = clientY - cy
+		let tiltx  = dy / cy
+		let tilty  = dx / cx
+		let radius = Math.sqrt(Math.pow(tiltx, 2) + Math.pow(tilty, 2))
+		let degree = radius * 12
+		gsap.to(`.${classes.wrapper}`, 1, { transform: `rotate3d( ${tiltx}, ${tilty}, 0, ${degree}deg )` })
 
-	// }
+	}
 	
-	// gsap.to('.card', { zoom: .98 })
-	// gsap.to('.l_main', { opacity: 1, duration: .1 })
-	// gsap.to('.l2_main', { opacity: 1, left: -10, top: 10, duration: .25, delay: .25 })
-	// gsap.to('.l3_main', { opacity: 1, left: -20, top: 20, duration: .25, delay: .25 })
-	// gsap.to('.card-russia', { opacity: .07, duration: .1 })
-	// gsap.to('.card-logo_w', { opacity: 1, duration: .225 })
-	// gsap.to('.card-chip', { opacity: 1, duration: .225 })
-	// gsap.to('.card-valid', { opacity: 1, zoom: 1, duration: .1, delay: .25 })
-  // gsap.to('.card-number-holder', { opacity: 1, zoom: 1, duration: .1, delay: .25 })
-  
+	gsap.to(`.${classes.link}`, { opacity: 1, delay: 1.4, duration: 2.5 })
+	gsap.to(`.${classes.title}`, { opacity: 1, delay: .4, duration: 2.5 })
+	gsap.to(`.${classes.text}`, { opacity: 1, delay: .9, duration: 3 })
+	gsap.to(`.${classes.image}`, { opacity: 1, left: 0, top: 0, duration: 2, delay: .25 })
 
-  return (
+	return (
     <div className={classes.wrapper}>
       <div className={classes.info}>
         <h2 className={classes.title}>hi</h2>
